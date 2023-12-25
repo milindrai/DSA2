f6cadabaf1861b47a983cbafd09973977d734fb3
Milind Rai <112194284+milindrai@users.noreply.github.com>
2023-12-25 14:08:42 +0530

    Update Sort Selection.cpp

--- Array Sorting/Sort Selection.cpp
+++ Array Sorting/Sort Selection.cpp
@@ -1,11 +1,19 @@
 //Selecting & swapping smallest element of unsorted/right part of array with the 1st element of unsorted array. [trick: sss:selection swapping smallest]
 //t.c=o(n^2) s.c=o(1)
-for(int i = 0; i < n-1 ; i++)  {
-    minimum = i ;
-    for(int j = i+1; j < n ; j++ ) {
-        if(A[ j ] < A[ minimum ])  { //finds the minimum element
-        minimum = j ;
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int n=nums.size();
+        
+        for(int i = 0; i < n-1 ; i++)  {
+            int minimum = i ;
+            for(int j = i+1; j < n ; j++ ) {
+                if(nums[ j ] < nums[ minimum ])  {
+                    minimum = j ;
+            }
+            }
+            swap (nums[minimum], nums[i]) ; 
         }
+        return nums;
     }
-    swap (A[minimum], A[i]) ; 
-}
+};
