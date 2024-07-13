86d633fd542512e5a24dc5a8926b4126cc995c7a
Milind Rai <112194284+milindrai@users.noreply.github.com>
2024-07-13 18:46:35 +0530

    Update Split into K or n+k subarrays with subarray with max sum minimized.cpp

--- Binary Search/Split into K or n+k subarrays with subarray with max sum minimized.cpp
+++ Binary Search/Split into K or n+k subarrays with subarray with max sum minimized.cpp
@@ -35,3 +35,29 @@ int minEatingSpeed(vector<int>& piles, int h) {
         
         return ans;
 }
+
+
+/*
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left=1;
+        int right=*max_element(piles.begin(),piles.end());
+        while(left<right){
+            int mid=left+(right-left)/2;
+            int hours=0;
+            for(int i=0;i<piles.size();i++){
+                hours+=ceil((double)piles[i]/mid);
+            }
+            if(hours>h){
+                left=mid+1;
+            }
+            else{
+                right=mid;
+            }
+        }
+        return left;
+    }
+};
+
+*/
