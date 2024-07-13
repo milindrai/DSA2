91ff48dc7ef1d4eba6f9b7696be98ff6a95c86be
Milind Rai <112194284+milindrai@users.noreply.github.com>
2024-07-13 12:25:42 +0530

    Update Search in Row and Column wise Sorted Matrix.cpp

--- Binary Search/Search in Row and Column wise Sorted Matrix.cpp
+++ Binary Search/Search in Row and Column wise Sorted Matrix.cpp
@@ -1,5 +1,27 @@
 // For fully sorted matrix i,e last element of 1st row is greater than 1st elem of 2nd rowTc thenn, search normally 0-n*m binaryt searach with index mat[/][%] =(log(n*m)) = o(logn + logm)
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int left = 0;
+        int right = n * m - 1;
 
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midElement = matrix[mid / m][mid % m];
+
+            if (midElement == target) {
+                return true;
+            } else if (midElement < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+};
 
 // For row wise & column wise sorted matrix : Start searching from mat[0][n-1] if(tar>mat) j--; else i++; =o(n) linear
 //       First find the row in which it can be by searching in logm 
